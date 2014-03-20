c7a07fd5dd1421b83c6ae4742df090d620f91577
Michael R. Burns <michael.burns@coxinc.com>
2014-03-20 15:05:11 -0400

    Power cycle and Genius Bar fix messages

--- helga_prod_fixer.py
+++ helga_prod_fixer.py
@@ -6,6 +6,8 @@ from helga.plugins import command
 RESPONSES = [
     'There is no hope for {thing}, {nick}',
     'It looks ok to me...',
+    'Did you power cycle {thing}? Are any of the lights blinking?',
+    'I\'ll take {thing} to the Genius Bar after work',
     'Turning {thing} off and back on again',
     'I really wish I could, but it looks past the point of no return',
 ]
