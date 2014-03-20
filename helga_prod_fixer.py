153a3f246156ec77934eb0c8a56d3c688cb758de
Michael R. Burns <michael.burns@coxinc.com>
2014-03-20 18:02:52 -0400

    Com Truise and don't use it fixer messages

--- helga_prod_fixer.py
+++ helga_prod_fixer.py
@@ -8,6 +8,8 @@ RESPONSES = [
     'It looks ok to me...',
     'Did you power cycle {thing}? Are any of the lights blinking?',
     'I\'ll take {thing} to the Genius Bar after work',
+    'Can we look at this tomorrow? I have Com Truise tickets...',
+    'Just tell them not to use {thing} for now.',
     'Turning {thing} off and back on again',
     'I really wish I could, but it looks past the point of no return',
 ]
